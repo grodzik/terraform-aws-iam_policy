e5a9f5e905af254049a6a758b8e83ae4c5835470
Paweł Tomak <pawel@tomak.eu>
2019-07-15 00:19:39 +0200

    Add more defaults to inputs

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_iam_policy" "policy" {
   name        = var.name
+  name_prefix = var.name_prefix
   path        = var.path
   description = var.description
   policy      = data.aws_iam_policy_document.policy.json
--- variables.tf
+++ variables.tf
@@ -1,15 +1,23 @@
 variable "description" {
-  description = "Policy description"
+  description = "Description of the IAM policy."
+  default     = null
   type        = string
 }
 
 variable "name" {
-  description = "Name of the policy"
+  description = "The name of the policy. If omitted, Terraform will assign a random, unique name."
+  default     = null
+  type        = string
+}
+
+variable "name_prefix" {
+  description = "Creates a unique name beginning with the specified prefix. Conflicts with name."
+  default     = null
   type        = string
 }
 
 variable "path" {
-  description = "Path of the policy"
+  description = "Path in which to create the policy. See IAM Identifiers for more information."
   default     = "/"
   type        = string
 }
